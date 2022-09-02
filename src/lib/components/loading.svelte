fa50514d2e184badc84aa369520f994ceeff2e98
Aliff Azmi <aliffazmi@Aliffs-MacBook-Air.local>
2022-09-02 22:28:41 +0800

    change loader

--- src/lib/components/loading.svelte
+++ src/lib/components/loading.svelte
@@ -1,8 +1,8 @@
 <script>
-	import { loading } from '$lib/loading';
-	import { Plane } from 'svelte-loading-spinners';
+  import { loading } from "$lib/loading";
+  import { Circle2 } from "svelte-loading-spinners";
 </script>
 
 {#if loading}
-	<Plane size="60" color="#14b8a6" unit="px" duration="2s" />
+  <Circle2 size="80" color="#14b8a6" ballTopLeft unit="px" duration="2s" />
 {/if}
