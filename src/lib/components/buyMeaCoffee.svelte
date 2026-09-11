8783731d1259ada9d7a50271c74f4e0c8d41666e
Aliff Azmi <aliff.azmi@revmedia.my>
2026-09-11 09:51:31 +0800

    fix: correct Belanja minum button markup

--- src/lib/components/buyMeaCoffee.svelte
+++ src/lib/components/buyMeaCoffee.svelte
@@ -10,10 +10,10 @@
   <a href="#" on:click={() => (showModal = true)}>
     <div class=" rounded border">
       <div class="p-4 bg-gradient-to-r from-teal-800 to-teal-500 hover:text-black">
-        <dib class="flex items-center gap-3 text-lg">
+        <div class="flex items-center gap-3 text-lg">
           <Icon icon="line-md:coffee-loop" class=" w-6 h-6" />
           <div class=" hidden md:block">Belanja minum</div>
-        </dib>
+        </div>
       </div>
     </div>
   </a>
